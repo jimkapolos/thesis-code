865e47213ede84a6cece6ec87dc08f078b446130
Dimitrios Brodimas <dimitris.brodimas@gmail.com>
2023-06-18 15:05:25 +0300

    FL - LSTM

---
+++ FL Benchmarking/LSTM - FL/functionFile.py
@@ -0,0 +1,64 @@
+#functionFile.py
+from numpy import array
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+
+def split_sequence(sequence, n_steps_in, n_steps_out):
+    X, y = list(), list()
+    for i in range(len(sequence)):
+        end_ix = i + n_steps_in
+        out_end_ix = end_ix + n_steps_out
+        if out_end_ix > len(sequence):
+            break
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix:out_end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return array(X), array(y)
+
+
+# print(train_seq.shape)
+# print(train_seq)
+def X_data_format(train_seq, test_seq, val_seq=[], n_steps_in=3, n_steps_out=2, n_features=1):
+    X_train, y_train = split_sequence(train_seq, n_steps_in, n_steps_out)
+    X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], n_features))
+    x_test, y_test = split_sequence(test_seq, n_steps_in, n_steps_out)
+    x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], n_features))
+    if val_seq == []:
+        x_val, y_val = [],[]
+    else:
+        x_val, y_val = split_sequence(val_seq, n_steps_in, n_steps_out)
+        x_val = x_val.reshape((x_val.shape[0], x_val.shape[1], n_features))
+    return X_train,y_train, x_test,y_test, x_val, y_val
+
+
+#
+# for i in range(len(X_train)):
+#     print(X_train[i], y_train[i])
+#
+# for i in range(len(x_test)):
+#     print(x_test[i], y_test[i])
+
+def simpleLSTM(X_train, n_neurons, y_train, x_test, y_test,n_epochs, n_steps_in, n_steps_out, n_features):
+    model = Sequential()
+    model.add(LSTM(n_neurons, activation='relu', return_sequences=False, input_shape=(n_steps_in, n_features)))
+    model.add(Dense(n_steps_out))
+    model.compile(optimizer='adam', loss='mse')
+    model.fit(X_train, y_train, epochs=n_epochs, verbose=0)
+    yhat = model.predict(x_test, verbose=0)
+    results = model.evaluate(x_test,y_test)
+    print("----", results)
+    return yhat
+
+
+# for i in range(len(y_test)):
+#     print(y_test[i], yhat[i], '---\n')
+def metrics_call(pred_type, y_test, yhat):
+    mse_metric = mean_squared_error(y_test, yhat)
+    mae_metric = mean_absolute_error(y_test, yhat)
+    r2_metric = r2_score(y_test, yhat, multioutput='variance_weighted')
+    print(pred_type, '--', "mse:", mse_metric, '--', "mae:", mae_metric,  '--', "r2:", r2_metric)
+
